83d04345dda1ea5dd66c55513317706a85cd3131
danila <you@example.com>
2024-08-12 23:13:26 +0300

    locked deps

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,9 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { setupStore } from "./store/store";
 import { Provider } from "react-redux";
+import './index.css'
 
 const store = setupStore();
-import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
