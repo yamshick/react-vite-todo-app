18042e0bd2102d1ffb94cdc2d1829b66e4e24d46
danila <you@example.com>
2024-08-13 10:36:32 +0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: '/dist',
+  plugins: [react(), viteSingleFile()],
 })
