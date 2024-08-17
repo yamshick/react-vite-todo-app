ca1d3fb6797b808a784f30b9c9ee2277261c08d3
danila <you@example.com>
2024-08-17 19:26:05 +0300

    dnd

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import { setupStore } from "./store/store";
 import { Provider } from "react-redux";
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 import './index.css'
 
 const store = setupStore();
@@ -10,7 +12,9 @@ const store = setupStore();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      <App />
+      <DndProvider backend={HTML5Backend}>
+        <App />
+      </DndProvider>
     </Provider>
   </StrictMode>,
 )
--- src/todo-list/todo/index.tsx
+++ src/todo-list/todo/index.tsx
@@ -1,23 +1,135 @@
-import React from "react"
-import { useDispatch } from "react-redux"
-import { ITodoItem, todosSlice } from "../../store/reducers/todos-slice"
-import './todo.css'
-import { STATUSES } from "../../constants"
-import { ButtonPanel } from "./button-panel"
+import type { Identifier, XYCoord } from 'dnd-core'
+import type { FC } from 'react'
+import { useRef } from 'react'
+import { useDrag, useDrop } from 'react-dnd'
 
-export const Todo = React.memo(({todo}: {todo: ITodoItem}) => {
-    const dispatch = useDispatch()
-    const {updateTodoStatus, deleteTodo} = todosSlice.actions
+export const ItemTypes = {
+    CARD: 'card',
+  }
 
-    const toggleStatus = () => {
-        dispatch(updateTodoStatus({id: todo.id, status: todo.status === STATUSES.DONE ? STATUSES.TODO : STATUSES.DONE}))
-    }
-    const onDelete = () => dispatch(deleteTodo(todo.id))
+const style = {
+  border: '1px dashed gray',
+  padding: '0.5rem 1rem',
+  marginBottom: '.5rem',
+  backgroundColor: 'white',
+  cursor: 'move',
+}
 
-return (<li>
-        <div className='todo'>
-            <p className={todo.status === STATUSES.DONE ? 'done-todo' : ''}>{todo.text}</p>
-            <ButtonPanel todoId={todo.id} toggleStatus={toggleStatus} onDelete={onDelete}/>
-        </div>
-    </li>)
-})
+export interface CardProps {
+  id: any
+  text: string
+  index: number
+  moveCard: (dragIndex: number, hoverIndex: number) => void
+}
+
+interface DragItem {
+  index: number
+  id: string
+  type: string
+}
+
+export const Card: FC<CardProps> = ({ id, text, index, moveCard }) => {
+  const ref = useRef<HTMLDivElement>(null)
+  const [{ handlerId }, drop] = useDrop<
+    DragItem,
+    void,
+    { handlerId: Identifier | null }
+  >({
+    accept: ItemTypes.CARD,
+    collect(monitor) {
+      return {
+        handlerId: monitor.getHandlerId(),
+      }
+    },
+    hover(item: DragItem, monitor) {
+      if (!ref.current) {
+        return
+      }
+      const dragIndex = item.index
+      const hoverIndex = index
+
+      // Don't replace items with themselves
+      if (dragIndex === hoverIndex) {
+        return
+      }
+
+      // Determine rectangle on screen
+      const hoverBoundingRect = ref.current?.getBoundingClientRect()
+
+      // Get vertical middle
+      const hoverMiddleY =
+        (hoverBoundingRect.bottom - hoverBoundingRect.top) / 2
+
+      // Determine mouse position
+      const clientOffset = monitor.getClientOffset()
+
+      // Get pixels to the top
+      const hoverClientY = (clientOffset as XYCoord).y - hoverBoundingRect.top
+
+      // Only perform the move when the mouse has crossed half of the items height
+      // When dragging downwards, only move when the cursor is below 50%
+      // When dragging upwards, only move when the cursor is above 50%
+
+      // Dragging downwards
+      if (dragIndex < hoverIndex && hoverClientY < hoverMiddleY) {
+        return
+      }
+
+      // Dragging upwards
+      if (dragIndex > hoverIndex && hoverClientY > hoverMiddleY) {
+        return
+      }
+
+      // Time to actually perform the action
+      moveCard(dragIndex, hoverIndex)
+
+      // Note: we're mutating the monitor item here!
+      // Generally it's better to avoid mutations,
+      // but it's good here for the sake of performance
+      // to avoid expensive index searches.
+      item.index = hoverIndex
+    },
+  })
+
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.CARD,
+    item: () => {
+      return { id, index }
+    },
+    collect: (monitor: any) => ({
+      isDragging: monitor.isDragging(),
+    }),
+  })
+
+  const opacity = isDragging ? 0 : 1
+  drag(drop(ref))
+  return (
+    <div ref={ref} style={{ ...style, opacity }} data-handler-id={handlerId}>
+      {text}
+    </div>
+  )
+}
+
+// import React from "react"
+// import { useDispatch } from "react-redux"
+// import { ITodoItem, todosSlice } from "../../store/reducers/todos-slice"
+// import './todo.css'
+// import { STATUSES } from "../../constants"
+// import { ButtonPanel } from "./button-panel"
+
+// export const Todo = React.memo(({todo}: {todo: ITodoItem}) => {
+//     const dispatch = useDispatch()
+//     const {updateTodoStatus, deleteTodo} = todosSlice.actions
+
+//     const toggleStatus = () => {
+//         dispatch(updateTodoStatus({id: todo.id, status: todo.status === STATUSES.DONE ? STATUSES.TODO : STATUSES.DONE}))
+//     }
+//     const onDelete = () => dispatch(deleteTodo(todo.id))
+
+// return (<li>
+//         <div className='todo'>
+//             <p className={todo.status === STATUSES.DONE ? 'done-todo' : ''}>{todo.text}</p>
+//             <ButtonPanel todoId={todo.id} toggleStatus={toggleStatus} onDelete={onDelete}/>
+//         </div>
+//     </li>)
+// })
